5ff37a17dda4c4ba80f7f79fa652eba76018ef0e
Mattrickx <ryguy.taff@gmail.com>
2017-01-17 09:16:19 -0600

    Update page.js

--- page.js
+++ page.js
@@ -1,2 +1,2 @@
-document.body.innerHTML='<div class="nav"><a href="//mattrick63.github.io"><li>Mattrick63</li></div> \
+document.body.innerHTML='<div class="nav"><a href="//mattrick63.github.io"><li>Mattrickx</li></div> \
 <div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//mattrickx.github.io/succe.css">'
