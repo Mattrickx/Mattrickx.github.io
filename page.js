3e3f18b8d1e8c7a662c9966e12e816bbebd63044
Mattrickx <ryguy.taff@gmail.com>
2017-01-18 08:16:03 -0600

    Update page.js

--- page.js
+++ page.js
@@ -1,2 +1,2 @@
-document.body.innerHTML='<div class="nav"><a href="//mattrickx.github.io"><li>Mattrick</li></a></div> \
+document.body.innerHTML='<div class="nav"><a href="//mattrickx.github.io"><li>Mattrick</li></a><a href="//mattrick.ml/games.html"><li>Games</li></a></div> \
 <div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//mattrickx.github.io/succe.css">'
